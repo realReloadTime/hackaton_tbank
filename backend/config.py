67422e03eae2da025b363b23ecc1d3d9463949eb
realReloadTime <max.shilcko@yandex.ru>
2025-06-17 16:56:51 +0700

    docker update+

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     API_DOMAIN: str = 'localhost'
     API_PORT: int = 8000
     TELEGRAM_BOT_TOKEN: str = '7867949246:AAFYbX8UvKQwLSsJbmLwepfZaAauzqHURuM'
-    TELEGRAM_WEBHOOK_URL: str = 'https://w7aa7t-176-59-144-238.ru.tuna.am'
+    TELEGRAM_WEBHOOK_URL: str = 'api.academus-pobeda.ru'
     WEB_APP_URL: str = 'localhost'
 
 
