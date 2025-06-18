9790e61b4f4d2e5b753e8d72e47bfd4ab13d8a03
realReloadTime <max.shilcko@yandex.ru>
2025-06-18 12:53:21 +0700

    автозаполнение тикеров

--- backend/config.py
+++ backend/config.py
@@ -8,11 +8,11 @@ class Settings(BaseSettings):
     API_PORT: int = 8000
 
     TELEGRAM_BOT_TOKEN: str = '7867949246:AAFYbX8UvKQwLSsJbmLwepfZaAauzqHURuM'
-    TELEGRAM_WEBHOOK_URL: str = 'bot2.academus-pobeda.ru'
+    TELEGRAM_WEBHOOK_URL: str = 'https://bot2.academus-pobeda.ru'
     TELEGRAM_PORT: int = 7000
     API_KEY_MISTRAL: str = "JfSjIKgF2WRAVSLiYcB3ZOuEQF05Asjf"
 
-    WEB_APP_URL: str = 'tbank2.academus-pobeda.ru'
+    WEB_APP_URL: str = 'https://tbank2.academus-pobeda.ru'
 
 
 settings = Settings()
